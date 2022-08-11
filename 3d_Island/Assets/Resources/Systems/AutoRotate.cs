47df131bb65357b9a1dd27d09aa204638c1ef28f
Mohamed Nour <71439813+MohamedNourSadek@users.noreply.github.com>
2022-08-11 18:45:52 +0200

    UI Plug-in

---
+++ 3d_Island/Assets/Resources/Systems/AutoRotate.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotate : MonoBehaviour
+{
+    [SerializeField] float speed = 2f;
+    [SerializeField] Vector3 defaultRotation = new Vector3();
+
+    void FixedUpdate()
+    {
+        this.transform.rotation = Quaternion.Euler(defaultRotation.x
+                                                 , defaultRotation.y + (speed * Time.realtimeSinceStartup),
+                                                   defaultRotation.zw);
+    }
+}
